631059e3f0cf762267c3bf6107e4de3cc5e24b52
JustinPead87 <justinpead@gmail.com>
2022-01-06 16:07:41 +0200

    first commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='configuration_manager',
+    version='0.01',
+    author='Justin Pead',
+    url='github.com',
+    py_modules=['configuration_manager'],
+    description='Configuration Management for Simple Deployment with Default options',
+    install_requires=['setuptools'],
+)
+
